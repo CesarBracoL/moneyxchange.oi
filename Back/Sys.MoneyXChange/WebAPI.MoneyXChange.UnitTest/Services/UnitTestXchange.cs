ff268d1f4fc2bf67621f77c95391d30f6426c528
Cesar Braco <cesarbracolujan@gmail.com>
2020-02-18 01:51:57 -0500

    second Commit project

---
+++ Back/Sys.MoneyXChange/WebAPI.MoneyXChange.UnitTest/Services/UnitTestXchange.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using WebAPI.MoneyXChange.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebAPI.MoneyXChange.Models;
+
+namespace WebAPI.MoneyXChange.Services.Tests
+{
+    [TestFixture()]
+    public class UnitTestXchange
+    {
+        [Test()]
+        public void ExchangeCurrencyTest()
+        {
+            // Arrange
+            var dbContext = NSubstitute.Substitute.For<StoreDBContext>();
+            var obj = NSubstitute.Substitute.For<OperationService>(dbContext);
+
+            // Act
+            var result  = obj.ExchangeCurrency(DateTime.Now, 1, 2, 50);
+
+            // Assert
+            Assert.AreEqual(result,0);
+        }
+    }
+}
